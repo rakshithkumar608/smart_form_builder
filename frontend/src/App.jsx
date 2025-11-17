5d44d6cc1453a1d66da2624b00e32f7f5bbf136f
Rakshitih <rakshithkumab01718@gmail.com>
2025-11-17 22:04:00 +0530

    Home page added to frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,27 @@
+import { Link, Route, Routes } from "react-router-dom";
 
-import { AuthProvider } from './contexts/AuthContext';
 const App = () => {
   return (
-   <AuthProvider>
-    
-   </AuthProvider>
-  )
-}
+    <div className="min-h-screen bg-gray-50">
+      <nav className="bg-white shadow p-4">
+        <div className="container mx-auto flex justify-between">
+          <Link to="/" className="font-bold">
+            SmartForms
+          </Link>
+          <div className="space-x-4">
+            <Link to="">Dashboard</Link>
+            <Link to="">Login</Link>
+          </div>
+        </div>
+      </nav>
 
-export default App
+      <main className="container mx-auto p-6">
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+      </main>
+    </div>
+  );
+};
+
+export default App;
