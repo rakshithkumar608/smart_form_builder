761c9fdf337a0d8057b704950450e8b36260b8aa
Rakshitih <rakshithkumab01718@gmail.com>
2025-11-18 17:07:16 +0530

    Changes made in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,44 +1,21 @@
-import { Link, Route, Routes } from "react-router-dom";
-import Home from './pages/Home';
-import Register from './pages/Register';
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
-import FormEditor from './pages/FormEditor';
+import { Route, Routes } from "react-router-dom";
+
+import Dashboard from "./pages/Dashboard";
+import FormEditor from "./pages/FormEditor";
 import FormPublic from "./pages/FormPublic";
-import Submission from "./pages/Submission";
-import Analytics from "./pages/Analytics";
 import { AuthProvider } from "./context/AuthContext";
 
 const App = () => {
   return (
     <AuthProvider>
-    <div className="min-h-screen bg-gray-50">
-      <nav className="bg-white shadow p-4">
-        <div className="container mx-auto flex justify-between">
-          <Link to="/" className="font-bold text-xl">
-            SmartForms
-          </Link>
-          <div className="space-x-4">
-            <Link to="/dashboard" className="p-2 border-red-300 border rounded-xl bg-red-400 hover:bg-red-500 shadow-emerald-700 font-semibold">Dashboard</Link>
-
-            <Link to="/login" className="p-2 border-green-300 border rounded-xl bg-green-400 hover:bg-green-500 shadow-emerald-700 font-semibold">Login</Link>
-          </div>
-        </div>
-      </nav>
-
-      <main className="container mx-auto p-6">
+      <div className="min-h-screen bg-gray-50">
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/register" element={<Register/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/dashboard" element={<Dashboard />}/>
-          <Route path="/forms/:id/edit" element={<FormEditor />}/>
-          <Route path="/forms/:slug" element={<FormPublic/>}/>
-          <Route path="/forms/:id/submission" element={<Submission />}/>
-          <Route path="/forms/:id/analytics" element={<Analytics />}/>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/forms/:id/edit" element={<FormEditor />} />
+          <Route path="/forms/:slug" element={<FormPublic />} />
         </Routes>
-      </main>
-    </div>
+      </div>
     </AuthProvider>
   );
 };
